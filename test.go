297f7b6c6252be2fdc465f11977e536223f57553
Rezaul Karim <rkarim923@gmail.com>
2024-05-08 22:27:33 +0600

    add new program file

---
+++ test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input1, input2, input3, input4, input5, input6 string
+	fmt.Scan(&input1, &input2, &input3, &input4, &input5, &input6)
+	fmt.Printf("Hello, World.\n%v %v %v %v %v %v", input1, input2, input3, input4, input5, input6)
+}
